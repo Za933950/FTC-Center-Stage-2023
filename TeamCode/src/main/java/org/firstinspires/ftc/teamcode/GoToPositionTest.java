01359e0e64f8f6e2fcca9c4145704580effbfb83
Zak Abji <za933950@gmail.com>
2023-12-30 12:01:48 -0500

    go to Position

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoToPositionTest.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.io.BufferedInputStream;
+
+@Autonomous(name="Odometry Test", group="Robot")
+
+public class GlobalCoordinateTracking extends LinearOpMode {
+    private DcMotor verticalLeft, verticalRight, horizontal;
+    String verticalLeftEncoderName = "frontLeft";
+    String verticalRightEncoderName = "frontRight";
+    String horizontalEncoderName = "intakeMotor";
+
+    public void runOpMode() {
+
+        verticalLeft = hardwareMap.dcMotor.get(verticalLeftEncoderName);
+        verticalRight = hardwareMap.dcMotor.get(verticalRightEncoderName);
+        horizontal = hardwareMap.dcMotor.get(horizontalEncoderName);
+
+        horizontal.setDirection(DcMotorSimple.Direction.REVERSE);
+        verticalRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        Odometry odometry = new Odometry(verticalLeft, verticalRight, horizontal);
+
+        waitForStart();
+
+        while (opModeIsActive()) {  
+
+            odometry.updatePosition();
+
+            telemetry.addData("XCoord", odometry.getXCoordinate());
+            telemetry.addData("YCoord", odometry.getYCoordinate());
+            telemetry.addData("Heading", odometry.getHeading());
+            telemetry.addData("Leftencoder", verticalLeft.getCurrentPosition());
+            telemetry.addData("Rightencoder", verticalRight.getCurrentPosition());
+            telemetry.addData("Middle1encoder", horizontal.getCurrentPosition());
+            telemetry.update();
+
+        }
+    }
+
+}
+
