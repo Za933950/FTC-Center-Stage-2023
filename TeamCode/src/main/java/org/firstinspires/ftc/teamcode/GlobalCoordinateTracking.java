249de425a0581876bdbbab11129965d614347ed6
Will <wkeenan895@gmail.com>
2024-01-06 16:56:29 -0500

    changes to the go to position on how it stops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GlobalCoordinateTracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GlobalCoordinateTracking.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,6 +8,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 import java.io.BufferedInputStream;
 
@@ -17,8 +23,22 @@ public class GlobalCoordinateTracking extends LinearOpMode {
     String verticalLeftEncoderName = "frontLeft";
     String verticalRightEncoderName = "frontRight";
     String horizontalEncoderName = "intakeMotor";
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
+    RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.UP;
+    RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+    IMU imu;
+
+    int logoFacingDirectionPosition;
+    int usbFacingDirectionPosition;
+    boolean orientationIsValid = true;
 
     public void runOpMode() {
+        imu = hardwareMap.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        imu.resetYaw();
+
+        boolean justChangedLogoDirection = false;
+        boolean justChangedUsbDirection = false;
 
         verticalLeft = hardwareMap.dcMotor.get(verticalLeftEncoderName);
         verticalRight = hardwareMap.dcMotor.get(verticalRightEncoderName);
@@ -35,6 +55,16 @@ public class GlobalCoordinateTracking extends LinearOpMode {
 
             odometry.updatePosition();
 
+            YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+            AngularVelocity angularVelocity = imu.getRobotAngularVelocity(AngleUnit.DEGREES);
+
+            telemetry.addData("Yaw (Z)", "%.2f Deg. (Heading)", orientation.getYaw(AngleUnit.DEGREES));
+            telemetry.addData("Pitch (X)", "%.2f Deg.", orientation.getPitch(AngleUnit.DEGREES));
+            telemetry.addData("Roll (Y)", "%.2f Deg.\n", orientation.getRoll(AngleUnit.DEGREES));
+            telemetry.addData("Yaw (Z) velocity", "%.2f Deg/Sec", angularVelocity.zRotationRate);
+            telemetry.addData("Pitch (X) velocity", "%.2f Deg/Sec", angularVelocity.xRotationRate);
+            telemetry.addData("Roll (Y) velocity", "%.2f Deg/Sec", angularVelocity.yRotationRate);
+
             telemetry.addData("XCoord", odometry.getXCoordinate());
             telemetry.addData("YCoord", odometry.getYCoordinate());
             telemetry.addData("Heading", odometry.getHeading());
@@ -45,6 +75,7 @@ public class GlobalCoordinateTracking extends LinearOpMode {
 
         }
     }
+
 
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoToPositionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoToPositionTest.java
@@ -1,20 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import org.firstinspires.ftc.teamcode.GoToPosition.*;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import PipeLines.BlueConeDetectionPipeline;
+import PipeLines.RedConeDetectionPipeline;
 
 
-@Autonomous(name="GTP Test", group="Robot")
+@Autonomous(name="Test Test", group="Robot")
+@Config
 
 public class GoToPositionTest extends LinearOpMode {
     private DcMotor verticalLeft, verticalRight, horizontal, leftBack, leftFront,rightFront, rightBack;
 
+
     String verticalLeftEncoderName = "frontLeft";
     String verticalRightEncoderName = "frontRight";
     String horizontalEncoderName = "intakeMotor";
+
+    OpenCvWebcam webcam;
+    public static double XPOSITION_1 = -5;
+    public static double YPOSITION_1 = -27;
+    public static double HEADING_1 = 0;
+    public static double STOP = 15;
+    public static double XPOSITION_2 = -5;
+    public static double YPOSITION_2 = -27;
+    public static double HEADING_2 = 0;
+    public static double XPOSITION_3 = -5;
+    public static double YPOSITION_3 = -27;
+    public static double HEADING_3 = 0;
+
+
+
+
 
     public void runOpMode() {
 
@@ -22,6 +49,7 @@ public class GoToPositionTest extends LinearOpMode {
         leftBack = hardwareMap.dcMotor.get("backLeft");
         rightFront = hardwareMap.dcMotor.get("frontRight");
         rightBack = hardwareMap.dcMotor.get("backRight");
+
 
         verticalLeft = hardwareMap.dcMotor.get(verticalLeftEncoderName);
         verticalRight = hardwareMap.dcMotor.get(verticalRightEncoderName);
@@ -32,15 +60,68 @@ public class GoToPositionTest extends LinearOpMode {
         rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
         Odometry odometry = new Odometry(verticalLeft, verticalRight, horizontal);
         Thread positionUpdate = new Thread(odometry);
+
         positionUpdate.start();
+
+        /*
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        RedConeDetectionPipeline samplePipeline = new RedConeDetectionPipeline(telemetry);
+        webcam.setPipeline(samplePipeline);
+
+
+        webcam.setMillisecondsPermissionTimeout(5000); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened()
+            {
+
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPSIDE_DOWN);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        int location = 0;
+
+
+        while (!opModeIsActive()) {
+            location = samplePipeline.getLocation();
+            if (location == 0) {
+                telemetry.addData("Left",0);
+            } if (location == 1) {
+                telemetry.addData("Center", 1);
+            } if (location == 2) {
+                telemetry.addData("Right", 2);
+            }
+            telemetry.update();
+        }*/
 
         waitForStart();
 
+
         if (opModeIsActive()) {
-            GoToPosition.goToPosition(24, 48,90,10, odometry, leftFront, leftBack, rightFront, rightBack, telemetry, 0.8);
-            GoToPosition.goToPosition(10,24,90, 10, odometry, leftFront, leftBack, rightFront, rightBack, telemetry, 0.8);
+
+                /*GoToPosition.goToPosition(0, -46,0,15, odometry, leftFront, leftBack, rightFront, rightBack, telemetry, .8);
+                GoToPosition.goToPosition(0, -46,190,15, odometry, leftFront, leftBack, rightFront, rightBack, telemetry, .8);*/
+            GoToPosition.goToPosition(XPOSITION_1, YPOSITION_1, HEADING_1, STOP, odometry, leftFront, leftBack, rightFront, rightBack, telemetry, .8);
+            GoToPosition.goToPosition(XPOSITION_2, YPOSITION_2, HEADING_2, STOP, odometry, leftFront, leftBack, rightFront, rightBack, telemetry, .8);
+            GoToPosition.goToPosition(XPOSITION_3, YPOSITION_3, HEADING_3, STOP, odometry, leftFront, leftBack, rightFront, rightBack, telemetry, .8);
+
+
+
+            /*GoToPosition.goToPosition(24, 48,90,15, odometry, leftFront, leftBack, rightFront, rightBack, telemetry, 1);
+            GoToPosition.goToPosition(0,0,0, 15, odometry, leftFront, leftBack, rightFront, rightBack, telemetry, 1);
+            sleep(5000);*/
 
         }
+
     }
 
 }
